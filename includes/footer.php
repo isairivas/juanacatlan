06f112e5586723115a0875f8538f492f92c50970
omar <omar@linux-391w.site>
2013-06-14 07:01:25 -0500

    vistas base

---
+++ includes/footer.php
@@ -0,0 +1,209 @@
+
+	<!-- start: Footer -->
+	<div id="footer">
+		
+		<!-- start: Container -->
+		<div class="container">
+			
+			<!-- start: Row -->
+			<div class="row">
+
+				<!-- start: About -->
+				<div class="span3">
+					
+					<h3>creativeLabs</h3>
+
+					<p>
+						<i class="mini-ico-map-marker mini-white"></i> 30 South Park Avenue
+					</p>
+					<p>	
+						<i class="mini-ico-map-marker mini-white"></i> San Francisco, CA 94108, USA
+					</p>	
+					<p>	
+						<i class="ico-z-iphone"></i> Phone: (123) 456-7890
+					</p>
+					<p>	
+						<i class="mini-ico-print mini-white"></i> Fax: +08 (123) 456-7890
+					</p>
+					<p>
+						<i class="mini-ico-envelope mini-white"></i> Email: contact@companyname.com
+					</p>
+					<p>
+						<i class="mini-ico-globe mini-white"></i> Web: companyname.com
+					</p>
+						
+				</div>
+				<!-- end: About -->
+
+				<!-- start: Latest Tweets -->
+				<div class="span3">
+					
+					<h3>Latest Tweets</h3>
+					
+					<ul id="twitter">
+						
+					</ul>
+
+					<div class="clear"></div>
+					
+					
+				</div>
+				<!-- end: Latest Tweets -->
+
+				<!-- start: Photo Stream -->
+				<div class="span3">
+					
+					<h3>Flickr Photo Stream</h3>
+					<div class="flickr-widget">
+						<script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=9&display=latest&size=s&layout=x&source=user&user=29609591@N08"></script>
+						<div class="clear"></div>
+					</div>
+					
+				</div>
+				<!-- end: Photo Stream -->
+				
+				<!-- start: Follow Us -->
+				<div class="span3">
+					
+					<h3>Follow Us</h3>
+					
+					<div id="social-r" class="tooltips">
+						<a href="#" rel="tooltip" title="Amazon" class="amazon">Amazon</a>
+						<a href="#" rel="tooltip" title="Behance" class="behance">Behance</a>
+						<a href="#" rel="tooltip" title="Blogger" class="blogger">Blogger</a>
+						<a href="#" rel="tooltip" title="DeviantART" class="deviantart">DeviantART</a>
+						<a href="#" rel="tooltip" title="Dribbble" class="dribbble">Dribbble</a>
+						<a href="#" rel="tooltip" title="Dropbox" class="dropbox">Dropbox</a>
+						<a href="#" rel="tooltip" title="Evernote" class="evernote">Evernote</a>
+						<a href="#" rel="tooltip" title="Facebook" class="facebook">Facebook</a>
+						<a href="#" rel="tooltip" title="Forrst" class="forrst">Forrst</a>
+						<a href="#" rel="tooltip" title="Github" class="github">Github</a>
+						<a href="#" rel="tooltip" title="Google Plus" class="googleplus">Google Plus</a>
+						<a href="#" rel="tooltip" title="Joli Cloud" class="jolicloud">Joli Cloud</a>
+						<a href="#" rel="tooltip" title="Last FM" class="last-fm">Last FM</a>
+						<a href="#" rel="tooltip" title="LinkedIn" class="linkedin">LinkedIn</a>
+						<a href="#" rel="tooltip" title="Picasa" class="picasa">Picasa</a>
+						<a href="#" rel="tooltip" title="Pintrest" class="pintrest">Pintrest</a>
+						<a href="#" rel="tooltip" title="RSS" class="rss">RSS</a>
+						<a href="#" rel="tooltip" title="Skype" class="skype">Skype</a>
+						<a href="#" rel="tooltip" title="Spotify" class="spotify">Spotify</a>
+						<a href="#" rel="tooltip" title="StumpleUpon" class="stumbleupon">StumbleUpon</a>
+						<a href="#" rel="tooltip" title="Tumblr" class="tumblr">Tumblr</a>
+						<a href="#" rel="tooltip" title="Twitter" class="twitter">Twitter</a>
+						<a href="#" rel="tooltip" title="Vimeo" class="vimeo">Vimeo</a>
+						<a href="#" rel="tooltip" title="WordPress" class="wordpress">WordPress</a>
+						<a href="#" rel="tooltip" title="Xing" class="xing">Xing</a>
+						<a href="#" rel="tooltip" title="Yahoo" class="yahoo">Yahoo</a>
+						<a href="#" rel="tooltip" title="YouTube" class="youtube">YouTube</a>
+						<a href="#" rel="tooltip" title="Soundcloud" class="soundcloud">Soundcloud</a>
+						<a href="#" rel="tooltip" title="myspace" class="myspace">myspace</a>
+						<a href="#" rel="tooltip" title="vkontakte" class="vkontakte">vkontakte</a>
+					</div>
+				
+				</div>
+				<!-- end: Follow Us -->
+				
+			</div>
+			<!-- end: Row -->	
+			
+		</div>
+		<!-- end: Container  -->
+
+	</div>
+	<!-- end: Footer -->
+	
+	<!-- start: Footer Menu -->
+	<div id="footer-menu" class="hidden-tablet hidden-phone">
+
+		<!-- start: Container -->
+		<div class="container">
+			
+			<!-- start: Row -->
+			<div class="row">
+
+				<!-- start: Footer Menu Logo -->
+				<div class="span2">
+					<div id="footer-menu-logo">
+						<a class="brand" href="#">Magnu<span>s</span>.</a>
+					</div>
+				</div>
+				<!-- end: Footer Menu Logo -->
+
+				<!-- start: Footer Menu Links-->
+				<div class="span9">
+					
+					<div id="footer-menu-links">
+
+						<ul id="footer-nav">
+
+							<li><a href="index.html">Start</a></li>
+
+							<li><a href="about.html">About</a></li>
+
+							<li><a href="services.html">Services</a></li>
+
+							<li><a href="pricing.html">Pricing</a></li>
+							
+							<li><a href="contact.html">Contact</a></li>
+
+						</ul>
+
+					</div>
+					
+				</div>
+				<!-- end: Footer Menu Links-->
+
+				<!-- start: Footer Menu Back To Top -->
+				<div class="span1">
+						
+					<div id="footer-menu-back-to-top">
+						<a href="#"></a>
+					</div>
+				
+				</div>
+				<!-- end: Footer Menu Back To Top -->
+			
+			</div>
+			<!-- end: Row -->
+			
+		</div>
+		<!-- end: Container  -->	
+
+	</div>	
+	<!-- end: Footer Menu -->
+
+	<!-- start: Copyright -->
+	<div id="copyright">
+	
+		<!-- start: Container -->
+		<div class="container">
+		
+			<div class="sixteen columns">
+				<p>
+					&copy; 2012, <a href="http://clabs.co">creativeLabs</a>. Designed by <a href="http://clabs.co">creativeLabs</a> in Poland <img src="img/poland.png" alt="Poland" style="margin-top:-4px">
+				</p>
+			</div>
+	
+		</div>
+		<!-- end: Container  -->
+		
+	</div>	
+	<!-- end: Copyright -->
+
+<!-- start: Java Script -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="assets/js/jquery-1.8.2.js"></script>
+<script src="assets/js/isotope.js"></script>
+<script src="assets/js/jquery.imagesloaded.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+<script src="assets/js/flexslider.js"></script>
+<script src="assets/js/carousel.js"></script>
+<script src="assets/js/jquery.cslider.js"></script>
+<script src="assets/js/slider.js"></script>
+<script src="assets/js/fancybox.js"></script>
+<script src="assets/js/twitter.js"></script>
+<script defer="defer" src="assets/js/custom.js"></script>
+<!-- end: Java Script -->
+
+</body>
+</html>
---
+++ includes/head.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- start: Meta -->
+	<meta charset="utf-8">
+	<title>Juanacatlan</title> 
+	<meta name="description" content="Magnus Bootstrap Theme"/>
+	<meta name="keywords" content="Template, Theme, web, html5, css3, Bootstrap" />
+	<meta name="author" content="Łukasz Holeczek from creativeLabs"/>
+	<!-- end: Meta -->
+	
+	<!-- start: Mobile Specific -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- end: Mobile Specific -->
+	
+	<!-- start: Facebook Open Graph -->
+	<meta property="og:title" content=""/>
+	<meta property="og:description" content=""/>
+	<meta property="og:type" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:image" content=""/>
+	<!-- end: Facebook Open Graph -->
+
+    <!-- start: CSS -->
+    <link href="..assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="assets/css/style.css" rel="stylesheet">
+	<link href="assets/css/parallax-slider.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Boogaloo">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Economica:700,400italic">
+	<!-- end: CSS -->
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+		<link href="css/ie.css" rel="stylesheet">
+     	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+	<!--[if IE 9]>
+    	<link href="css/ie9.css" rel="stylesheet">
+    <![endif]-->
+
+</head>
