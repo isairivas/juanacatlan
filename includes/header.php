2375c9e01738c7e9a768ac90860d1a94100ffcac
omar <omar@linux-391w.site>
2013-06-14 07:28:14 -0500

    <body> en header

--- includes/header.php
+++ includes/header.php
@@ -1,46 +1,47 @@
-<header>		
-	<!--start: Container -->
-	<div class="container">
-		
-		<!--start: Navigation -->
-		<div class="navbar navbar-inverse">
-    		<div class="navbar-inner">
-          		<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            		<span class="icon-bar"></span>
-            		<span class="icon-bar"></span>
-            		<span class="icon-bar"></span>
-          		</a>
-				<a class="brand" href="#"><i class="ico-thin-right-arrow ico-color circle"></i> Magnu<span>s</span>.</a>
-          		<div class="nav-collapse collapse">
-            		<ul class="nav">
-						<li class="active"><a href="index.html">Home</a></li>
-              			<li><a href="about.html">About</a></li>
-						<li class="dropdown">
-                			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Features<b class="caret"></b></a>
-                			<ul class="dropdown-menu">
-                  				<li><a href="social-icons.html">Social Icons</a></li>
-								<li><a href="icons.html">Icons</a></li>
-								<li><a href="typography.html">Typography</a></li>
-								<li><a href="shortcodes.html">Shortcodes</a></li>
-								<li><a href="list-styles.html">List Styles</a></li>
-                			</ul>
-              			</li>
-						<li class="dropdown">
-                			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio<b class="caret"></b></a>
-                			<ul class="dropdown-menu">
-                  				<li><a href="portfolio3.html">3 Columns</a></li>
-								<li><a href="portfolio4.html">4 Columns</a></li>
-                			</ul>
-              			</li>									
-						<li><a href="services.html">Services</a></li>
-						<li><a href="pricing.html">Pricing</a></li>
-						<li><a href="blog.html">Blog</a></li>
-              			<li><a href="contact.html">Contact</a></li>
-            		</ul>
-          		</div>
-        	</div>
-      	</div>	
-		<!--end: Navigation -->								
-	</div>
-	<!--end: Container-->			
-</header>
+<body>
+	<header>		
+		<!--start: Container -->
+		<div class="container">
+			
+			<!--start: Navigation -->
+			<div class="navbar navbar-inverse">
+	    		<div class="navbar-inner">
+	          		<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+	            		<span class="icon-bar"></span>
+	            		<span class="icon-bar"></span>
+	            		<span class="icon-bar"></span>
+	          		</a>
+					<a class="brand" href="#"><i class="ico-thin-right-arrow ico-color circle"></i> Magnu<span>s</span>.</a>
+	          		<div class="nav-collapse collapse">
+	            		<ul class="nav">
+							<li class="active"><a href="index.html">Home</a></li>
+	              			<li><a href="about.html">About</a></li>
+							<li class="dropdown">
+	                			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Features<b class="caret"></b></a>
+	                			<ul class="dropdown-menu">
+	                  				<li><a href="social-icons.html">Social Icons</a></li>
+									<li><a href="icons.html">Icons</a></li>
+									<li><a href="typography.html">Typography</a></li>
+									<li><a href="shortcodes.html">Shortcodes</a></li>
+									<li><a href="list-styles.html">List Styles</a></li>
+	                			</ul>
+	              			</li>
+							<li class="dropdown">
+	                			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio<b class="caret"></b></a>
+	                			<ul class="dropdown-menu">
+	                  				<li><a href="portfolio3.html">3 Columns</a></li>
+									<li><a href="portfolio4.html">4 Columns</a></li>
+	                			</ul>
+	              			</li>									
+							<li><a href="services.html">Services</a></li>
+							<li><a href="pricing.html">Pricing</a></li>
+							<li><a href="blog.html">Blog</a></li>
+	              			<li><a href="contact.html">Contact</a></li>
+	            		</ul>
+	          		</div>
+	        	</div>
+	      	</div>	
+			<!--end: Navigation -->								
+		</div>
+		<!--end: Container-->			
+	</header>
