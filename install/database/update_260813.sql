fadd5322974a9332fd98081378373668548d5cd4
isairivas <isairivas@gmail.com>
2013-08-26 22:53:07 -0500

    - se añadiola tabla de noticias

--- install/database/update_260813.sql
+++ install/database/update_260813.sql
@@ -1,1 +1,12 @@
 ALTER TABLE  `transparencia` ADD  `is_subcategoria` ENUM(  'TRUE',  'FALSE' ) NULL AFTER  `categoria_transparencia_id` ;
+CREATE TABLE IF NOT EXISTS `noticias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
+  `contenido` text,
+  `fecha` date NOT NULL,
+  `status` enum('ACTIVO','INACTIVO') NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
