3e146dbfd6b6ac194c30a6e3d3500f5f6a85ab67
malchiel <malchiel@Xubuntu-Mlk>
2013-09-01 00:41:40 -0500

    Mensaje Bienvenida

--- src/modules/frontend/views/index/index.php
+++ src/modules/frontend/views/index/index.php
@@ -2,7 +2,9 @@
         <div id="da-slider" class="da-slider">
             <div class="da-slide">
                 <!--Borrale aquí, puto!-->
-                <h2>Sitio en Construcción</h2>
+                <h2>Seguimos trabajando</h2>
+                <h3>Bienvenido al nuevo portal del Ayuntamiento</h3>
+                <h4>Sitio en mantenimiento</h4>
                 <div class="da-img"><img src="<?php echo HOME; ?>/assets/magnus/img/construccion.jpg" alt="construcción" /></div>
             </div>
         </div>
