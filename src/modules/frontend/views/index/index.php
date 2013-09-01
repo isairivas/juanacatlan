217d98dd2f4691ed51112b6649f2e0f6e6d08770
malchiel <malchiel@Xubuntu-Mlk>
2013-09-01 01:35:59 -0500

    Mensaje Bienvenida

--- src/modules/frontend/views/index/index.php
+++ src/modules/frontend/views/index/index.php
@@ -3,6 +3,7 @@
             <div class="da-slide">
                 <!--Borrale aquí, puto!-->
                 <h2>Seguimos trabajando</h2>
+              
                 <div class="da-img"><img src="<?php echo HOME; ?>/assets/magnus/img/construccion.jpg" alt="construcción" /></div>
             </div>
         </div>
